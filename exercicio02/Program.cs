04bae1db4263e58cf4218350c8cc973e919c0b24
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-04-17 16:34:54 -0300

    Add files via upload

---
+++ exercicio02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+        casas decimais conforme exemplos.
+        Fórmula da área: area = π . raio2
+        Considere o valor de π = 3.14159 */
+
+        System.Console.Write("Digite o valor do raio para calcular a área: ");
+
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double PI = 3.14159;
+
+        double calculo = (Math.Pow(raio, 2.0)) * PI;
+
+        System.Console.WriteLine($"O valor da área será: {calculo.ToString("f4", CultureInfo.InvariantCulture)}");
+
+
+
+
+
+
+
+
+    }
+}
