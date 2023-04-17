bcd8742ad00a8d46c9109a58399304ff7c2ffca9
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-04-17 16:34:11 -0300

    Add files via upload

---
+++ exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // FAÇA UM PROGRAMA QUE LEIA DOIS NUMEROS INTEIROS E MOSTRE A SOMA 
+
+        Console.Write("Digite o primeiro número: ");
+
+        int numero1 = int.Parse(Console.ReadLine());
+
+        System.Console.Write("Digite o segundo numero: ");
+
+        int numero2 = int.Parse(Console.ReadLine());
+
+
+        int soma = numero1 + numero2;
+
+        System.Console.WriteLine($"A soma dos valores digitados, é: {soma}");
+    }
+}
