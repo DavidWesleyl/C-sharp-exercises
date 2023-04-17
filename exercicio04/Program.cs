6511912ddc32c870f80f18c2e590ce45fe46118e
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-04-17 16:37:21 -0300

    Add files via upload

---
+++ exercicio04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por 
+        hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas 
+        decimais */
+
+        System.Console.Write("Digite o numero do Funcionário: ");
+
+        int numero_do_funcionario = int.Parse(Console.ReadLine());
+
+
+        System.Console.Write("Quantas horas trabalhadas?: ");
+
+        int horas_trabalhadas = int.Parse(Console.ReadLine());
+
+
+        System.Console.Write("Quanto o funcionário ganha por hora? R$ ");
+
+        double ganho_hora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        double calculo = ganho_hora * horas_trabalhadas;
+
+        System.Console.WriteLine(" ");
+
+        System.Console.WriteLine($" O funcionário num {numero_do_funcionario}, ganha aproximadamente R$ {calculo.ToString("f2", CultureInfo.InvariantCulture)}");
+
+
+
+    }
+}
