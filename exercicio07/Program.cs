9832a2b0be7ab6dea2e01059b9d99abf993d52f8
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-04-20 22:06:22 -0300

    Add files via upload

---
+++ exercicio07/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+        System.Console.Write("Digite um número para saber se ele é negativo ou não: ");
+
+        int numero = int.Parse(Console.ReadLine());
+
+
+        if (numero < 0)
+        {
+            System.Console.WriteLine($"O número {numero} é negativo");
+        }
+
+        else
+        {
+            System.Console.WriteLine($"O numero {numero} é Positivo! ");
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
