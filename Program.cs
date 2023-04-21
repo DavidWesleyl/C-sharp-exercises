ba403a508acf200106679305882f2689cbbfc280
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-04-20 22:04:46 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,32 @@
-﻿using System;
-using System.Globalization;
-internal class Program
+﻿internal class Program
 {
-    private static void Main()
+    private static void Main(string[] args)
     {
-        System.Console.Write("Entre com o seu nome completo: "); // Nome
+        // Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
 
-        string nome = Console.ReadLine();
+        System.Console.Write("Digite um número para saber se ele é negativo ou não: ");
 
-
-        System.Console.Write("Quantos cômodos tem na susa casa?: "); // Comodos
-
-        int comodos = int.Parse(Console.ReadLine());
+        int numero = int.Parse(Console.ReadLine());
 
 
-        System.Console.Write("Entre com o preço de um produto: "); // Preço do produto
+        if (numero < 0)
+        {
+            System.Console.WriteLine($"O número {numero} é negativo");
+        }
 
-        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        else
+        {
+            System.Console.WriteLine($"O numero {numero} é Positivo! ");
 
-
-        // Output
-        System.Console.WriteLine(" ");
-        System.Console.WriteLine($"Seu nome é {nome}");
-        System.Console.WriteLine($"Sua casa tem {comodos} comodos.");
-        System.Console.WriteLine($"Preço digitado: {preco.ToString(CultureInfo.InvariantCulture)}");
-
-        System.Console.WriteLine("------------------------------------------------------------------------------------------------------");
+        }
 
 
 
-        System.Console.WriteLine("Entre com seu ultimo nome, idade e altura (Mesma linha)"); // Todas as informações armazenadas em um vetor
-
-        string[] informacoes = Console.ReadLine().Split(' '); // -> Irá ler as informações de variáveis diferentes na mesma linha 
-
-        string Nome_do_usuario = informacoes[0];
-        int idade_do_usuario = int.Parse(informacoes[1]);
-        double altura_do_usuario = double.Parse(informacoes[2], CultureInfo.InvariantCulture);
 
 
-        System.Console.WriteLine(" ");
-        System.Console.WriteLine(Nome_do_usuario);
-        System.Console.WriteLine(idade_do_usuario);
-        System.Console.WriteLine(altura_do_usuario.ToString(CultureInfo.InvariantCulture));
+
 
 
 
     }
-
 }
