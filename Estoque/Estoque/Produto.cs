05657f2f17dca7f38de081439de266be29b60ec7
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-05-11 11:55:41 -0300

    Add files via upload

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Estoque
+{
+    internal class Produto
+    {
+
+        public string Nome;
+        public double Valor;
+        public int Quantidade;
+    
+    
+        public double ValorTotalemEstoque() // Primeiro Método
+        {
+            return Valor * Quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade) // O método Void Significa que não irá retornar valor, Nesse caso ele irá adicionar valores no Programa Principal
+        {
+            Quantidade += quantidade;
+
+        }
+
+        public void RemoverProdutos(int remocao) // Esse Método irá remover valores no Programa principal
+        {
+            Quantidade -= remocao;
+        }
+
+
+
+
+
+
+        public override string ToString()
+        {
+            return $" Produto: {Nome}, Valor: {(Valor).ToString("f2", CultureInfo.InvariantCulture)}, Quantidade: {Quantidade}, Total {(Valor * Quantidade).ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Estoque/Estoque/Program.cs
@@ -0,0 +1,83 @@
+﻿using Estoque;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Faça um program que leia os dados de um produto em estoque (Nome, Preço e quantidade em estoque). Em seguida: 
+          
+        -> Mostre os dados do Produto ( Nome, preço, quantidade e quantidade total)
+        -> Realize uma entrada no Estoque e mostre novamente4 os resultados 
+        -> Realize uma saída no estoque e mostre novamente os produtos 
+                                                                                                                    */
+        
+        Produto itens;
+
+        itens = new Produto();
+
+        Console.WriteLine("    D A D O S  D O  P R O D U T O    ");
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine(" ");
+
+        Console.Write("Produto: ");
+        itens.Nome = Console.ReadLine();
+
+
+        Console.Write("Valor R$: ");
+        itens.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        Console.Write("Quantidade: ");
+        itens.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(" ");
+
+
+        Console.WriteLine($"Relação: {itens}");
+
+        Console.WriteLine(" ");
+        
+        Console.Write("Digite a quantidade de produtos a ser adicionada no estoque: ");
+        int Adicao_de_Produtos = int.Parse(Console.ReadLine());
+        itens.AdicionarProdutos(Adicao_de_Produtos);
+
+        Console.WriteLine(" ");
+        Console.WriteLine($"Relação Atualizada >> {itens}");
+
+        
+        Console.WriteLine(" ");
+        
+        Console.Write("Digite a Quantidade de Produtos a ser removida do estoque: ");
+        int RemocaodeProdutos = int.Parse(Console.ReadLine());
+        itens.RemoverProdutos(RemocaodeProdutos);
+
+        
+        Console.WriteLine(" ");
+        Console.WriteLine($"Relação Atualizada >> {itens}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
