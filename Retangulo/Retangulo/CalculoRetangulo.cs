5360c1d6f32093abc4113fb902f01d04030bf7e3
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-05-11 14:26:37 -0300

    Add files via upload

---
+++ Retangulo/Retangulo/CalculoRetangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo
+{
+    internal class CalculoRetangulo
+    {
+        public double Altura;
+        public double Largura;
+
+
+        public double Area()
+        {
+            return Altura * Largura; // Para calcular a Área de um retângulo, basta multiplicas a Base X Altura
+
+
+
+        }
+
+        public double Perimetro()
+        {
+            return (Altura + Largura) * 2.0; // Para calcular o Perimetro tem que somar a Base+ Altura e multiplicar por 2. P = 2 (b * a)
+        }
+
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2)); // Para calcular a Diagonal de um retangulo é A Raiz quadrada de B ^ 2 + A ^ 2
+        }
+
+        
+
+    }
+}
---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Retangulo;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        /* Fazer um programa para ler os valores da largura e altura de um retângulo. Em 
+           seguida, mostrar na tela o valor de sua área, perímetro e diagonal. */
+
+        /* Neste exercicio foi criada uma Classe chamada CalculoRetangulo, onde recebe 2 atributos (Altura, Largura) 
+           e foram criados 3 métodos para calcular Àrea, Perímetro e Largura */
+
+
+        CalculoRetangulo valores;
+
+        valores = new CalculoRetangulo();
+
+        Console.WriteLine("Digite a altura e largura do Retangulo, abaixo: ");
+        valores.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        valores.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Area = {(valores.Area()).ToString("f2", CultureInfo.InvariantCulture)}");
+
+        Console.WriteLine();
+        Console.WriteLine($"Perimetro = {(valores.Perimetro()).ToString("f2", CultureInfo.InvariantCulture)}");
+
+        Console.WriteLine();
+        Console.WriteLine($"Largura = {valores.Diagonal()}");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
