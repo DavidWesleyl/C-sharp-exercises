e98cf8d828ccc9e3559e57a50898fb511ecd09e5
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-05-15 17:36:22 -0300

    Add files via upload

---
+++ ConversaoMoeda/ConversaoMoeda/Dolar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace ConversaoMoeda
+{
+    internal class Dolar
+    {
+
+        public static double ValorDolar; 
+
+        public static double Quantidade;
+
+
+
+        public static double Calculo() // Quantidade de dólares
+        {
+
+            return ValorDolar * Quantidade;             
+        
+        }
+
+        public static double IOF() // Imposto de 6% sobre o valor em dolar
+        {
+            return 0.06 * Calculo();
+        }
+      
+
+        public static double Total() // Total a ser pago
+        {
+            return Calculo() + IOF();
+        }
+        
+
+       
+
+
+    
+    
+    
+    
+    }
+}
---
+++ ConversaoMoeda/ConversaoMoeda/Program.cs
@@ -0,0 +1,54 @@
+﻿using ConversaoMoeda;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Faça um programa para ler a cotação do Dolar e depois um valor em dolares a ser comprado em reais.
+         * Informe quantos reais a pessoa vai pagar pelos dolares, considerando ainda que a pessoa terá que pagar 6% de IOF 
+         sobre o valor en dolar. OBS: Crie uma classe para calcular */
+
+        
+        // Comentário: Neste exercicios  foi usado métodos estáticos.
+
+        // Como estamos usando Métodos estáticos (Static), não precisamos instanciar a Classe. Podemos chama-lá direto.
+
+        Console.Write("Qual é a cotação do Dolar: ");
+        Dolar.ValorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+
+        Console.Write("Quanto em dolar você vai comprar? ");
+        Dolar.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.WriteLine($"Valor a ser pago em reais = R$ {Dolar.Total().ToString("f2", CultureInfo.InvariantCulture)}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
