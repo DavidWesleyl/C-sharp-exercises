de1eab16452502f325250eddb305eaf643de22d9
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-06-02 15:07:51 -0300

    Add files via upload

---
+++ AlugueldeQuarto/Aluguel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace AlugueldeQuarto
+{
+	internal class Aluguel
+	{
+		public string Nome { get; set; } // Nome do usuário
+		public string Email { get; private set; } // Email do usuário privado, sem risco de alterações
+
+
+
+		public Aluguel() // Construtor padrão
+		{
+
+		}
+
+		public Aluguel(string nome, string email) : this() // Sobrecarga01
+		{
+			Nome = nome;
+			Email = email;
+		}
+
+
+
+		public override string ToString() // Método to String com as informações.
+		{
+			return $" Nome: {Nome} | Email: {Email}";
+		}
+
+
+
+	}
+}
