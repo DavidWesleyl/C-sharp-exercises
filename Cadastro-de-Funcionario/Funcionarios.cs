9e2170fe664df61753bc2729104178463cae9714
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-06-15 14:30:14 -0300

    Add files via upload

---
+++ Cadastro-de-Funcionario/Funcionarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cadastro_de_Funcionario
+{
+	internal class Funcionarios
+	{
+		// Atributos
+
+		public int ID_Funcionario { get; private set; }
+		public string Nome_Funcionario { get; set; }
+		public double Salario_Funcionario { get; set; }
+
+
+		public Funcionarios(int id, string nome, double salario)
+		{
+			ID_Funcionario = id;
+			Nome_Funcionario = nome;
+			Salario_Funcionario = salario;
+		}
+
+		public void AumentoSalarial(double porcentagem)
+		{
+			Salario_Funcionario += Salario_Funcionario * (porcentagem / 100.00);
+		}
+
+		public override string ToString()
+		{
+			return $"ID: {ID_Funcionario} | Nome: {Nome_Funcionario} | Salario R$: {Salario_Funcionario.ToString("f2", CultureInfo.InvariantCulture)}";
+		}
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ Cadastro-de-Funcionario/Program.cs
@@ -0,0 +1,75 @@
+﻿using Cadastro_de_Funcionario;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		/*	Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de
+			N funcionários. Não deve haver repetição de id.
+			Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
+			Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma
+			mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários,
+			conforme exemplos.
+			Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa
+			ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de
+			aumento por porcentagem dada */
+
+
+		Console.Write("Quanto funcionários serão registrados? ");
+		int quantidade = int.Parse(Console.ReadLine());
+
+		List<Funcionarios> dados_funcionarios = new List<Funcionarios>();
+
+		for (int inicio = 0; inicio < quantidade; inicio++)
+		{
+			Console.WriteLine();
+
+			Console.WriteLine($"Funcionario #{inicio + 1}");
+
+			Console.Write("Digite o ID: ");
+			int id = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Nome: ");
+			string nome = Console.ReadLine();
+
+			Console.Write("Salario R$: ");
+			double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			dados_funcionarios.Add(new Funcionarios(id, nome, salario));
+
+		}
+
+		Console.Write("Digite o ID do funcionário que irá receber o aumento: ");
+		int id_do_funcionario = int.Parse(Console.ReadLine());
+
+		Funcionarios id_escolhido = dados_funcionarios.Find(valor => valor.ID_Funcionario == id_do_funcionario);
+
+		if (id_escolhido != null)
+		{
+
+			Console.Write("Digite a porcentagem do aumento: ");
+			double aumento_do_funcionario = double.Parse(Console.ReadLine());
+
+			id_escolhido.AumentoSalarial(aumento_do_funcionario);
+		}
+		else
+		{
+			Console.WriteLine("O ID Digitado não existe!");
+		}
+
+		Console.WriteLine("Lista Atualizada: ");
+
+		foreach (Funcionarios cadastrados in dados_funcionarios)
+		{
+			Console.WriteLine(cadastrados);
+
+
+		}
+
+
+
+	}
+}
