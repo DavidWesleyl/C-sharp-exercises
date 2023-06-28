8c21a0675da36362752244f7b74de37edc3b4709
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-06-28 12:47:38 -0300

    Update Program.cs

--- Analise-de-Matriz/Program.cs
+++ Analise-de-Matriz/Program.cs
@@ -6,8 +6,8 @@
 		 uma matriz de M linhas por N colunas contendo números inteiros,
 		 podendo haver repetições. Em seguida, ler um número inteiro X que
 		 pertence à matriz. Para cada ocorrência de X, mostrar os valores à
-		 esquerda, acima, à direita e abaixo de X, quando houver, conforme
-		 exemplo.*/
+		 esquerda, acima, à direita e abaixo de X.
+		 */
 
 
 		Console.Write("Digite a quantidade de Linhas: "); // Captura a quantidade de Linhas
